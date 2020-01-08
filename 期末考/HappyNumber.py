ebe27371f78e4912ec15ccaa094ccde56a011b7b
luke90329 <32509228+luke90329@users.noreply.github.com>
2020-01-09 01:24:41 +0800

    Update HappyNumber.py

--- 期末考/HappyNumber.py
+++ 期末考/HappyNumber.py
@@ -1,10 +1,12 @@
 def f(n):
     while n != 1:
-        count = 0
+        sum = 0
         for i in str(n):
-            count += int(i)**2
-        n = count
+            sum += int(i)**2
+        n = sum
         if n == 4:
             print('no')
             return
     print('yes')
+num = 10
+f(num)
